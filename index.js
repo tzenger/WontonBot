1885c53a31af8a8943dfc0cf61e9b7235b29ace4
Matthew Tzeng <matthewtzeng88@gmail.com>
2020-08-16 16:20:50 -0500

    fixed some privacy issues

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 // require the discord.js module
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
+/**
 // require data values stored in the config.json file
 const config = require('./config.json');
+**/
+
+// destructures the config file values into values which can be used in index.js
+const {prefix, token} = require('./config.json')
+
 
 // create a new Discord client
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Ready!')
 });
-
-
 
 
 // logs any sent message into cmd prompt
 client.on('message', message => {
-	console.log(message.content);
+	console.log(message.content)
 });
 
 // endconvo (responds to endconvo message with "sounds intriguing, well anyways I gtg see ya")
 client.on('message', message => {
-	if(message.content === config.prefix + "endconvo") {
+	if(message.content === prefix + "endconvo") {
         message.channel.send("sounds intriguing, well anyways I gtg see ya!")
     }
 });
 
-
 // login to Discord with your app's token
-client.login(config.token);
+client.login(token)
 
 
