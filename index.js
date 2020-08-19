094cec1e728c0e6866aa7fbc1d6a37a27a346d1f
Matthew Tzeng <matthewtzeng88@gmail.com>
2020-08-18 21:44:21 -0500

    Added the ability for users to have a user pinged in bot messages without pinging their account with the command

--- index.js
+++ index.js
@@ -12,55 +12,108 @@ const {prefix, token} = require('./config.json')
 // create a new Discord client
 const client = new Discord.Client()
 
-function checkAt(msg) {
-    if(msg.indexOf("@") != -1) {
-        return true
-    }
-    return false
-}
-
 //reading help file framework
-var fs = require('fs');
+var fs = require('fs')
 var commandList = fs.readFileSync('commandList.txt', 'utf8')
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('Ready!')
-});
+})
 
 // logs any sent message into cmd prompt
 client.on('message', message => {
 	console.log(message.content)
-});
+})
+
+//FUNCTIONS FOR CODING ----------------
 
 
-//listening for any of the messages down below
-client.on('message', message => {
+function checkAt(msg) { // a function to check for the presence of an @, which is commonly associated with a user tag
+    if(msg.indexOf("@") != -1)
+        return true
+    return false
+}
 
-    // help (prints the help menu txt thingy)
-    if(message.content === `${prefix}help`) {
+function isolateTrailText(fulltext, initcmd) { // isolates the trailing text after a command
+    return fulltext.slice(initcmd.length + 1, fulltext.length)
+}
+
+function txt2User(txt, guild) { // returns promise with the resulting user's data
+    return guild.members.fetch({query: txt, limit: 1})
+}
+
+client.on('message', message => { //listening for any of the messages down below
+
+
+//COMMUNICATION -----------------
+    if(message.content.includes(`${prefix}ping`) && !checkAt(message.content)) { // ping (pings a specified person on the server)
+        txt2User(isolateTrailText(message.content, `${prefix}ping`), message.guild)
+        .then(res => { // extracts the user's id and sends it as a tagged message
+            let guildMember = res
+            console.log(guildMember.first().id)
+            message.channel.send(`<@${guildMember.first().id}>`)
+        })
+        .catch(console.error)
+    }
+    
+
+    if(message.content === `${prefix}help`) { // help (prints the help commandList.txt)
         message.channel.send(commandList)
         }
+    
+//MUSIC ----------------------
 
-    // endconvo (responds to endconvo message with "sounds intriguing, well anyways I gtg see ya")
-	if(message.content === `${prefix}endconvo`) {
-        console.log("no @")
+       if(message.content === `${prefix}play`) {
+        message.channel.send('I would love to talk to you more about this!')
+    } 
+
+//FUN ------------------------
+    
+    
+
+	if(message.content === `${prefix}startconvo`) { // startconvo
+        message.channel.send('I would love to talk to you more about this!')
+    }
+    else if (message.content.includes(`${prefix}startconvo`) && checkAt(message.content)) {
+             message.channel.send('I would love to talk to you, ' + message.content.slice(message.content.indexOf('@') - 1, message.content.indexOf('>') + 1) + ', more about this!')
+     }
+    else if (message.content.includes(`${prefix}startconvo`) && !checkAt(message.content)) {
+        txt2User(isolateTrailText(message.content, `${prefix}startconvo`), message.guild)
+        .then(res => { // extracts the user's id and sends it as a tagged message
+            let guildMember = res
+            console.log(guildMember.first().id)
+            message.channel.send('I would love to talk to you, ' + `<@${guildMember.first().id}>` + ', more about this!')
+        })
+        .catch(console.error)
+    }
+    
+
+	if(message.content === `${prefix}endconvo`) { // endconvo
         message.channel.send('Sounds intriguing, but I gtg so see ya!')
     }
     else if (message.content.includes(`${prefix}endconvo`) && checkAt(message.content)) {
              message.channel.send('Sounds intriguing ' + message.content.slice(message.content.indexOf('@') - 1, message.content.indexOf('>') + 1) + ', but I gtg so see ya!')
      }
+    else if (message.content.includes(`${prefix}endconvo`) && !checkAt(message.content)) {
+        txt2User(isolateTrailText(message.content, `${prefix}endconvo`), message.guild)
+        .then(res => { // extracts the user's id and sends it as a tagged message
+            let guildMember = res
+            console.log(guildMember.first().id)
+            message.channel.send('Sounds intriguing ' + `<@${guildMember.first().id}>` + ', but I gtg so see ya!')
+        })
+        .catch(console.error)
+    }
     
 
-    // fish (blubs))
-    if(message.content === `${prefix}fish`) {
+    if(message.content === `${prefix}fish`) { // fish (blubs)
         for(i = 0; i < 5; i++)
         message.channel.send('blub')
     }
 
-    // github (sends user link to official WontonBot github)
-    if(message.content === `${prefix}github`) {
+
+    if(message.content === `${prefix}github`) { // github (sends user link to official WontonBot github)
         message.channel.send("https://github.com/tzenger/WontonBot")
         }
     
@@ -69,5 +122,3 @@ client.on('message', message => {
 
 // login to Discord with your app's token
 client.login(token)
-
-
